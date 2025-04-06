07e47aca8ccb286dadbeb5634e67544a58fcb503
batool04 <64125668+batool04@users.noreply.github.com>
2025-04-07 00:13:39 +0300

    Add files via upload

---
+++ 16-Bit RISC Processor Design/Instruction_Memory.v
@@ -0,0 +1,12 @@
+`include "Parameter.v" module 
+Instruction_Memory( 
+input[15:0] pc, 
+output[15:0] instruction 
+); 
+reg [`col - 1:0] memory [`row_i -
+1:0]; wire [3 : 0] rom_addr = pc[4 : 1]; initia
+begin 
+$readmemb("D:/test/test.prog", memory,0,14); 
+end 
+assign instruction = memory[rom_addr]; 
+endmodule
